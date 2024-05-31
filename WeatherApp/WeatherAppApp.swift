a4f8a8ad8522314a16d123455aa969bcad5a692d
tanaka <tanaka.mawoyo@yahoo.com>
2024-05-31 15:31:52 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tanaka Mawoyo on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
